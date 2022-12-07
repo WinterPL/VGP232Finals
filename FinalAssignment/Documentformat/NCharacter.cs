cdcc1f0503af27ec1474acce31d8294dedd2876a
WinterPL <95836645+WinterPL@users.noreply.github.com>
2022-12-07 10:21:32 -0800

    add file

---
+++ FinalAssignment/Documentformat/NCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documentformat
+{
+    public class NCharacter
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Origin { get; set; } = string.Empty;
+        public string kind { get; set; } = string.Empty;
+        public string sex { get; set; } = string.Empty;
+        public string Appearence { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ FinalAssignment/FinalAssignment/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using Documentformat;
+
+namespace FinalAssignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public NCharacter nCharacter = new NCharacter();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Do you want to save changes?";
+            string caption = "Save";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+
+            result = System.Windows.MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+
+            switch (result)
+            {
+                case MessageBoxResult.Cancel:
+                    break;
+                case MessageBoxResult.Yes:
+                    Save_Click(sender, e);
+                    setAllblank();
+                    break;
+                case MessageBoxResult.No:
+                    setAllblank();
+                    break;
+            }
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            LoadButton_Click(sender, e);
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveButton_Click(sender,e);
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            nCharacter.Name = Name.Text;
+            nCharacter.Origin = origin.Text;
+            nCharacter.kind = Kind.Text;
+            nCharacter.sex = Sex.Text;
+            nCharacter.Appearence = appearence.Text;
+            nCharacter.Description = Description.Text;
+
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.Filter = "Xml files (*.xml)|*.xml";
+            if (saveFileDialog.ShowDialog() == true)
+                using (FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Append))
+                {
+                    XmlSerializer xs = new XmlSerializer(typeof(NCharacter));
+                    xs.Serialize(fs, nCharacter);
+                }
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "Xml files (*.xml)|*.xml";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true) { 
+            using (FileStream fs = new FileStream(dlg.FileName, FileMode.OpenOrCreate))
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(NCharacter));
+                nCharacter = new NCharacter();
+                nCharacter = (NCharacter)xs.Deserialize(fs);
+            }
+            }
+            Name.Text = nCharacter.Name;
+            origin.Text = nCharacter.Origin;
+            Kind.Text = nCharacter.kind;
+            Sex.Text = nCharacter.sex;
+            appearence.Text = nCharacter.Appearence;
+            Description.Text = nCharacter.Description;
+            Title = "CharacterDocument - " + dlg.Title;
+        }
+
+        private void setAllblank()
+        {
+            nCharacter.Name = " ";
+            nCharacter.Origin = " ";
+            nCharacter.kind = " ";
+            nCharacter.sex = " ";
+            nCharacter.Appearence = " ";
+            nCharacter.Description = " ";
+        }
+    }
+}
